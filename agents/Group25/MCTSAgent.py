9534b765aa1beed4b0664557b4abd4fa3d5a6832
b03663pl <pak.lam-2@student.manchester.ac.uk>
2025-11-28 15:42:23 +0000

    Fixed bugs

--- agents/Group25/MCTSAgent.py
+++ agents/Group25/MCTSAgent.py
@@ -1,3 +1,4 @@
+from __future__ import annotations
 import subprocess
 
 from src.Colour import Colour
@@ -34,8 +35,8 @@ class Node:
 
     def legal_moves(self, board: Board):
         moves = []
-        for y, row in enumerate(board._tiles):
-            for x, tile in enumerate(row):
+        for x, col in enumerate(board.tiles):
+            for y, tile in enumerate(col):
                 if tile.colour is None:
                     moves.append(Move(x, y))
         return moves
@@ -72,7 +73,6 @@ class Node:
         # While the game hasn't ended, keep on playing random moves 
         # TODO: Add a heuristic to select non-random moves
 
-        # Check the game hasn't ended as a result of the last move, hence the opposite(self.colour)
         # TODO: again deepcopy here is very slow
         new_board = deepcopy(self.board)
         colour = self.colour
@@ -89,8 +89,6 @@ class Node:
             new_board.set_tile_colour(move.x, move.y, colour)
             colour = Colour.opposite(colour)
 
-        return Colour.opposite(colour)
-
     def backpropagate(self, winner: Colour):
         self.visits += 1 
         if winner == self.root_colour:
@@ -100,7 +98,8 @@ class Node:
 
     # TODO: make search time based to fit with time constraints of CW
     def search(self, iterations):
-        for _ in range(iterations):
+        for i in range(iterations):
+            print("Iteration", i)
             node = self.select()
             if node.untried_moves:
                 node = node.expand()
@@ -115,14 +114,14 @@ class MCTSAgent(AgentBase):
         # Colour: red or blue - red moves first.
         super().__init__(colour)
 
-        self.agent_process = subprocess.Popen(
-            ["./agents/MCTSAgent/mcts-hex"],
-            stdout=subprocess.PIPE,
-            stdin=subprocess.PIPE,
-            stderr=subprocess.PIPE,
-            text=True,
-            bufsize=1
-        )
+        # self.agent_process = subprocess.Popen(
+        #     ["./agents/MCTSAgent/mcts-hex"],
+        #     stdout=subprocess.PIPE,
+        #     stdin=subprocess.PIPE,
+        #     stderr=subprocess.PIPE,
+        #     text=True,
+        #     bufsize=1
+        # )
 
     def make_move(self, turn: int, board: Board, opp_move: Move | None) -> Move:
         """The game engine will call this method to request a move from the agent.
@@ -140,31 +139,32 @@ class MCTSAgent(AgentBase):
             Move: The agent's move
         """
 
-        rows = board.tiles
-        board_strings = []
-        for row in rows:
-            row_string = ""
-            for tile in row:
-                colour = tile.colour
-                if colour is None:
-                    t = "0"
-                else:
-                    t = colour.get_char()
-                row_string += t
-            board_strings.append(row_string)
-        board_string = ",".join(board_strings)
+        # rows = board.tiles
+        # board_strings = []
+        # for row in rows:
+        #     row_string = ""
+        #     for tile in row:
+        #         colour = tile.colour
+        #         if colour is None:
+        #             t = "0"
+        #         else:
+        #             t = colour.get_char()
+        #         row_string += t
+        #     board_strings.append(row_string)
+        # board_string = ",".join(board_strings)
+        #
+        # if opp_move is None:
+        #     command = f"START;;{board_string};{turn};"
+        # elif opp_move.x == -1 and opp_move.y == -1:
+        #     command = f"SWAP;;{board_string};{turn};"
+        # else:
+        #     command = f"CHANGE;{opp_move.x},{opp_move.y};{board_string};{turn};"
+        #
+        # self.agent_process.stdin.write(command + "\n")
+        # self.agent_process.stdin.flush()
 
-        if opp_move is None:
-            command = f"START;;{board_string};{turn};"
-        elif opp_move.x == -1 and opp_move.y == -1:
-            command = f"SWAP;;{board_string};{turn};"
-        else:
-            command = f"CHANGE;{opp_move.x},{opp_move.y};{board_string};{turn};"
-
-        self.agent_process.stdin.write(command + "\n")
-        self.agent_process.stdin.flush()
-
-        response = self.agent_process.stdout.readline().rstrip()
+        root = Node(self.colour, opp_move, None, board, self.colour)
+        iterations = 20
+        response = root.search(iterations)
         # assuming the response takes the form "x,y" with -1,-1 if the agent wants to make a swap move
-        x, y = map(int, response.split(","))
-        return Move(x, y)
+        return response
